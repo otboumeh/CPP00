e04d61d28511f99b27ca2f97a5daaafa12e9f46c
vboxuser <otmane.boumehdi1@gmail.com>
2025-04-15 08:23:55 +0000

    first

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,11 @@
+ #include <iostream>
+
+int main(int argc , char **argv){ 
+    if (arg < 2){
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+        return 0;
+    }
+    else{
+            
+    }
+  } 
