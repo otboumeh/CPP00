2c2cf2feab7daa9198243476dac577c4489e17e6
Otmane Boumehdi <otboumeh@c3r6s6.42urduliz.com>
2025-04-15 11:06:01 +0200

    ex 00 hecho

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,11 +1,18 @@
  #include <iostream>
 
 int main(int argc , char **argv){ 
-    if (arg < 2){
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
-        return 0;
+    if (argc < 2){
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     }
     else{
-            
+        for (int i = 1 ;argv[i];i++){
+            for (int y = 0; y < argv[i][y]; y++)
+            {
+                std::cout << static_cast<char>(toupper(argv[i][y]));
+            }
+            std::cout << " ";
+        }
     }
-  } 
+    std::cout << "\n";
+    return 0;
+}
