e650a459243cc87211a07ef40986a6ec28d3a141
vboxuser <otmane.boumehdi1@gmail.com>
2025-04-18 19:20:07 +0000

    ex1

---
+++ ex01/PhoneBook.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+class PhoneBook{
+    
+    public :
+};
+int main (int argc , char **argv){
+    (void)argv;
+    if (argc < 2 || argc >3)
+            return 1;
+    while (true)
+    {
+                std::cout << "hello";
+    }
+
+}
